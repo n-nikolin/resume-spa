8e6f322eb7ffa574b362c164e6a80ccbf3481a85
Nikita N <nikitanikolin85@gmail.com>
2022-07-19 02:10:35 +0300

    scroll to top button appears only when window is scrolled past a certain component + some css tweaking

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,16 +1,29 @@
 import upArrow from "../assets/arrow_up.svg";
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > document.getElementById('about-me-container-content')) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    });
+  }, []);
+
   return (
+
     <div className="btn scroll-to-top">
-    <button
-      onClick={() => {
-        window.scrollTo({ top: 0, behaviour: "auto" });
-      }}
-    >
-      <img src={upArrow} alt="up chevron" />
-    </button>
-  </div>
-  )
+      {isVisible && <button
+        onClick={() => {
+          window.scrollTo({ top: 0, behaviour: "auto" });
+        }}
+      >
+        <img src={upArrow} alt="up chevron" />
+      </button>}
+    </div>
+  );
 }
