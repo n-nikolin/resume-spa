ccf5d20273eb5ac81aa2f94c0c3124a22e4ad813
Nikita N <nikitanikolin85@gmail.com>
2022-10-12 13:44:44 +0300

    project caard styling done. maybe do something about the colors

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -2,10 +2,27 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 
 export default function MyProjects(props) {
+  function truncateString(string) {
+    const truncated = string.split(" ").slice(0, 10).join(" ") + "...";
+    return truncated;
+  }
   return (
     <section className="my-projects" id="my_projects">
       <h2>{props.myProjects.heading}</h2>
-      <ProjectCard />
+      <div className="project-cards">
+      {props.myProjects.projects.map((project, index) => {
+          return (
+            <div className="project-card" key={index}>
+              <ProjectCard
+                id={project.id}
+                title={project.title}
+                shortDescription={truncateString(project.description)}
+                longDescription={project.description}
+              />
+            </div>
+          );
+        })}
+        </div>
     </section>
   );
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProjectCardModal from './ProjectCardModal'
 
-export default function ProjectCard() {
+export default function ProjectCard(props) {
   return (
-    <div className="project-card">ProjectCard
-    <ProjectCardModal />
+    <div className="project-card">
+    <h3>{props.title}</h3>
+    <p>{props.shortDescription}</p>
+    <ProjectCardModal/>
     </div>
   )
 }
