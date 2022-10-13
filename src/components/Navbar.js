6929a6ec97798edc538b39d945dfe0918141d461
Nikita N <nikitanikolin85@gmail.com>
2022-10-13 17:00:37 +0300

    added visibility class/id and some styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
-import React from "react";
+// TODO: make navbar visibility toggleable
+// by adding and removing element ids or classes
+
+import { useState } from "react";
+import hamburgerMenu from "../assets/hamburgerMenu.svg";
 
 export default function Navbar(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleVisible = (e) => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
   const menuItems = Object.entries(props.navbar).map(([key, value]) => (
     <li key={key}>
       <a href={"#" + key}>{value}</a>
     </li>
   ));
+
   return (
     <div className="navbar">
-      <ul>{menuItems}</ul>
+      <button onClick={toggleVisible}>
+        <img src={hamburgerMenu} alt="hamburger menu icon" />
+      </button>
+      <ul id="closed">{menuItems}</ul>
     </div>
   );
 }
