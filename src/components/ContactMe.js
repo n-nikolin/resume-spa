f5a2f1bbfb6a26302cfa881078bf640a5d2a4e82
Nikita N <nikitanikolin85@gmail.com>
2022-10-21 21:38:57 +0300

    adden send email and changed outline to border

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,34 +1,59 @@
-import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default function ContactMe({contactMe}) {
+
+export default function ContactMe({ contactMe }) {
+  const form = useRef();
+  // const serviceID = process.env.REACT_APP_SERVICE_ID
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <section className="contact-me" id="contact_me">
       <h2 className="contact-me heading">{contactMe.heading}</h2>
-      <form action="submit">
+      <form ref={form} onSubmit={sendEmail}>
         <legend>
+          
           <h3>{contactMe.legend.heading}</h3>
           <p>{contactMe.legend.legend}</p>
         </legend>
         <div>
           <label htmlFor="">
             {contactMe.label.name}
-            <input type="text" />
+            <input name="user_name" type="text" />
           </label>
         </div>
         <div>
           <label htmlFor="">
             {contactMe.label.email}
-            <input type="text" />
+            <input name="email" type="text" />
           </label>
         </div>
         <div>
           <label htmlFor="">
             {contactMe.label.message}
-            <textarea />
+            <textarea name="message" />
           </label>
         </div>
         <div>
-        <button type="submit">{contactMe.submit_btn}</button>
+          <button type="submit">{contactMe.submit_btn}</button>
         </div>
       </form>
     </section>
