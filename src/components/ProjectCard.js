f6254e5ce75b961da8a8375fb8cdf7a9371e773d
Nikita N <nikitanikolin85@gmail.com>
2022-10-23 19:51:13 +0300

    modal closes when clicking the cross button

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,18 +3,23 @@ import ProjectCardModal from "./ProjectCardModal";
 
 export default function ProjectCard(props) {
   const [isOpen, setIsOpen] = useState(false);
-  const handleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+
   return (
-    <div className="project-card" onClick={handleOpen}>
+    <div
+      className="project-card"
+      onClick={() => {
+        setIsOpen(true);
+        console.log("m");
+      }}
+    >
       <h3>{props.title}</h3>
       <p>{props.shortDescription}</p>
       <ProjectCardModal
-        className={isOpen ? "open" : "closed"}
         id={props.id}
         title={props.title}
         description={props.longDescription}
+        modalOpen={isOpen}
+        setModalOpen={setIsOpen}
       />
     </div>
   );
--- src/components/ProjectCardModal.js
+++ src/components/ProjectCardModal.js
@@ -1,26 +1,37 @@
 import React from "react";
-import logo from '../assets/logo'
-import ui from '../assets/ui'
+import logo from "../assets/logo";
+import ui from "../assets/ui";
 
-export default function ProjectCardModal(props) {
+export default function ProjectCardModal({
+  id,
+  title,
+  description,
+  modalOpen,
+  setModalOpen,
+}) {
   return (
-    <div className={`project-card-modal ${props.className}`} id={props.id}>
+    <div
+      className={`project-card-modal ${modalOpen ? "open" : "closed"}`}
+      id={id}
+      onClick={() => setModalOpen(false)}
+    >
+      {console.log(modalOpen)}
       <div className="container">
-      <div className="content" onClick={(e) => e.stopPropagation()}>
-        <button>
-          <img src={ui.cross} alt="" />
-        </button>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-        <a
-          href="https://github.com/n-nikolin"
-          //to open in new tab
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo.github} alt="github logo" />
-        </a>
-      </div>
+        <div className="content" onClick={(e) => e.stopPropagation()}>
+          <button onClick={() => setModalOpen(false)}>
+            <img src={ui.cross} alt="" />
+          </button>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <a
+            href="https://github.com/n-nikolin"
+            //to open in new tab
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo.github} alt="github logo" />
+          </a>
+        </div>
       </div>
     </div>
   );
