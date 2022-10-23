dc76be91f4fd561928b849760e9536e5e0b00bd6
Nikita N <nikitanikolin85@gmail.com>
2022-10-23 17:21:24 +0300

    added a success popup

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,26 @@
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
+// TODO: try to clear form when message has been sent
 
+import { useRef, useState, useEffect } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactMe({ contactMe }) {
   const form = useRef();
-  // const serviceID = process.env.REACT_APP_SERVICE_ID
+  const [isMessageSent, setIsMessageSent] = useState(false);
+  const [messageForm, setMessageForm] = useState({
+    user_name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setMessageForm({
+      ...messageForm,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const sendEmail = (e) => {
+    setIsMessageSent(true);
     e.preventDefault();
     emailjs
       .sendForm(
@@ -25,37 +39,64 @@ export default function ContactMe({ contactMe }) {
       );
   };
 
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setIsMessageSent(false);
+    }, 2500);
+    return () => {
+      clearTimeout(timeId);
+    };
+  });
+
   return (
     <section className="contact-me" id="contact_me">
       <h2 className="contact-me heading">{contactMe.heading}</h2>
       <form ref={form} onSubmit={sendEmail}>
         <legend>
-          
           <h3>{contactMe.legend.heading}</h3>
           <p>{contactMe.legend.legend}</p>
         </legend>
         <div>
           <label htmlFor="">
             {contactMe.label.name}
-            <input name="user_name" type="text" />
+            <input
+              type="text"
+              name="user_name"
+              value={messageForm.user_name}
+              onChange={handleChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="">
             {contactMe.label.email}
-            <input name="email" type="text" />
+            <input
+              name="email"
+              type="text"
+              value={messageForm.email}
+              onChange={handleChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="">
             {contactMe.label.message}
-            <textarea name="message" />
+            <textarea
+              name="message"
+              value={messageForm.message}
+              onChange={handleChange}
+            />
           </label>
         </div>
         <div>
           <button type="submit">{contactMe.submit_btn}</button>
         </div>
       </form>
+      {isMessageSent && (
+        <div className="success-popup content">
+          <h3>{contactMe.success_popup}</h3>
+        </div>
+      )}
     </section>
   );
 }
