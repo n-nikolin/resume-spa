50f3cb6be0efd329209a3e3d79e101cfcd449d99
Nikita N <nikitanikolin85@gmail.com>
2022-11-04 13:00:48 +0300

    fixed profile pic issue and began working on fetching data from github

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,14 +1,33 @@
-import React from "react";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 import ProjectCard from "./ProjectCard";
 
 export default function MyProjects({ myProjects }) {
+  // const [projectData, setProjectData] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://api.github.com/users/n-nikolin/repos", {
+  //       headers: {
+  //         authorization: "token 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",
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setProjectData(res.data);
+  //     });
+  // }, []);
+
   function truncateString(string) {
     const truncated = string.split(" ").slice(0, 10).join(" ") + "...";
     return truncated;
   }
+
   return (
     <section className="my-projects" id="my_projects">
       <h2>{myProjects.heading}</h2>
+      {/* {projectData.map((item, index) => {
+        return <p key={index}>{item.name}</p>;
+      })} */}
       <div className="project-cards">
         {myProjects.projects.map((project, index) => {
           return (
